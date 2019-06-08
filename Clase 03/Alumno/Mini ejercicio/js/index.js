61043aa638bd38b043142096197ee4f36e90c747
Yanina <segattiyanina@gmail.com>
2019-06-08 12:29:27 -0300

    Modicar comentario

--- Clase 03/Alumno/Mini ejercicio/js/index.js
+++ Clase 03/Alumno/Mini ejercicio/js/index.js
@@ -1,6 +1,6 @@
 var pass = prompt('Ingrese pass')
 
-// mientras pass sea vacio  ==='', tengo que pedir el password de vuelta
+// mientras pass sea vacio  ==='', tengo que pedi el password de vuelta
 while (pass === '' || pass === '1234') {
   pass = prompt('Ingrese pass')
 }
