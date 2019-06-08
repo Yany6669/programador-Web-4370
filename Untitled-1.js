236a5c7e751be645cd9de01e2e469335799dbdda
Yanina <segattiyanina@gmail.com>
2019-06-08 15:17:46 -0300

    update files

---
+++ Untitled-1.js
@@ -0,0 +1,27 @@
+switch (operacion) {
+  case 'suma':
+    var op1 = parseInt(prompt('ingrese operando 1'))
+    var op1 = parseInt(prompt('ingrese operando 2'))
+    console log(op1 + op2)
+    break
+  case 'division':
+    var op1 = parseInt(prompt('ingrese operando 1'))
+    var op1 = parseInt(prompt('ingrese operando 2'))
+
+    while = (op2 === 0)
+console.log(op1 / op2)
+    break
+}
+
+
+
+
+var daysOfTheWeek = [
+  'Lunes';
+'Martes';
+'Miércoles';
+
+*******
+
+for (var i = 1; i < daysOfTheWeek.lenght; i += 1)
+
